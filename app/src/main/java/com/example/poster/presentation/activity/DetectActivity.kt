b6b13af349f048b6b07cd52a56aaf86838c2c26b
urusbievv <urusbievalbert89@mail.ru>
2024-05-22 05:02:05 +0300

    add comment

--- app/src/main/java/com/example/poster/presentation/activity/DetectActivity.kt
+++ app/src/main/java/com/example/poster/presentation/activity/DetectActivity.kt
@@ -36,27 +36,30 @@ class DetectActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
         picture = findViewById(R.id.button)
 
+
+        // проверка разрешение камеры
         picture.setOnClickListener {
-            // Launch camera if we have permission
             if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 startActivityForResult(cameraIntent, 1)
             } else {
-                // Request camera permission if we don't have it
                 requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
             }
         }
     }
 
     private fun classifyImage(image: Bitmap) {
+        // Создание новой модели
         val model = Model.newInstance(applicationContext)
 
+        // Создание буфера для входных данных модели
         val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
 
+        // Создание буфера для конвертации пикселей изображения в числовые значения
         val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
         byteBuffer.order(ByteOrder.nativeOrder())
 
-
+        // Получение цветовых значений пикселей изображения и конвертация их в числовые значения
         val intValues = IntArray(imageSize * imageSize)
         image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
         var pixel = 0
@@ -68,12 +71,14 @@ class DetectActivity : AppCompatActivity() {
                 byteBuffer.putFloat((value and 0xFF) * (1f / 255f))
             }
         }
-
+        // Загрузка данных в буфер
         inputFeature0.loadBuffer(byteBuffer)
 
+        // Обработка данных моделью и получение результатов
         val outputs = model.process(inputFeature0)
         val outputFeature0 = outputs.outputFeature0AsTensorBuffer
 
+        // Определение класса с максимальной оценкой
         val confidences = outputFeature0.floatArray
         var maxPos = 0
         var maxConfidence = 0f
@@ -83,26 +88,34 @@ class DetectActivity : AppCompatActivity() {
                 maxPos = i
             }
         }
+        // Задание результата
         val classes = arrayOf("Albert", "Phone")
         result.text = classes[maxPos]
 
+        // Формирование строки с оценкой для каждого класса
         var s = ""
         for (i in classes.indices){
             s += String.format("%s: %1f%%\n", classes[i],confidences[i] * 100)
         }
+        // отображение оценки
         confidence.text = s
+
+        // Закрытие модели для освобождения ресурсов
         model.close()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == 1 && resultCode == RESULT_OK) {
-            // Handle camera image result here
+            // Обработка результата изображения с камеры
             var image: Bitmap = data?.extras?.get("data") as Bitmap
             val dimension = Math.min(image.width,image.height)
             image = ThumbnailUtils.extractThumbnail(image,dimension,dimension)
             imageView.setImageBitmap(image)
 
+            // Масштабирование изображения до размера, ожидаемого моделью
             image = Bitmap.createScaledBitmap(image,imageSize,imageSize,false)
+
+            // Классификация изображения
             classifyImage(image)
         }
         super.onActivityResult(requestCode, resultCode, data)
